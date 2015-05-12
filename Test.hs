0d26f09ebb6d58d6d5a98aa7e9e5c7f19df8f54a
Viraj Sinha <virajosinha@gmail.com>
2015-05-12 11:05:21 -0400

    cleaned up testing setup

--- Test.hs
+++ Test.hs
@@ -7,45 +7,38 @@ import Text.ParserCombinators.Parsec
 
 import BaristaParser
 
-pTest msg e = TestCase . either (assertFailure . show) (assertEqual msg e)
+-- create a parse test given an info message, parser, and tuple of (inputToTest, desiredOuput)
+ptest msg prsr testcase = TestCase . either (assertFailure . show) 
+                                            (assertEqual msg $ snd testcase)
+                             $ (show <$> parse prsr "" (fst testcase))
 
-runParse r e = show <$> parse r "" e
-
-numberedIngredients = unlines [
-     "Recipe IcedMocha\r",
-     "1- 60`mL Chocolate\r",
-     "2- 60`mL Espresso [mix]\r",
-     "3- 30`mL Milk\r"]
-
-----
-simpleExample = unlines [
+simpleExample = (
+     unlines [
      "Recipe Mocha\r",
      "- 30`mL Steamed_Milk\r",
      "- 60`mL Chocolate\r",
-     "- 60`mL Espresso\r"]
+     "- 60`mL Espresso\r"],
+     "Recipe {recipeName = \"Mocha\", ingredients = [Ingredient {volume = 30, measure = \"ml\", ingredientName = \"Steamed_Milk\", index = Nothing},Ingredient {volume = 60, measure = \"ml\", ingredientName = \"Chocolate\", index = Nothing},Ingredient {volume = 60, measure = \"ml\", ingredientName = \"Espresso\", index = Nothing}]}")
 
-simpleExample_output = 
-     "Recipe {recipeName = \"Mocha\", ingredients = [Ingredient {volume = 30, measure = \"ml\", ingredientName = \"Steamed_Milk\", index = Nothing},Ingredient {volume = 60, measure = \"ml\", ingredientName = \"Chocolate\", index = Nothing},Ingredient {volume = 60, measure = \"ml\", ingredientName = \"Espresso\", index = Nothing}]}"
+test_simpleInput = ptest "for the first recipe in the language spec" 
+                         recipe 
+                         simpleExample
 
-test_simpleInput = pTest "for the first recipe in the language spec" 
-     simpleExample_output $ runParse recipe simpleExample
+test_ingredient = ptest "for valid single non-indexed ingredient" 
+                         ingredient 
+                         ( "- 30`mL Steamed_Milk\r\n", "Ingredient {volume = 30, measure = \"ml\", ingredientName = \"Steamed_Milk\", index = Nothing}")
 
+test_ingredientIndexed = ptest "for valid single indexed ingredient" 
+                         ingredient
+                         ("1- 30`mL Steamed_Milk\r", "Ingredient {volume = 30, measure = \"ml\", ingredientName = \"Steamed_Milk\", index = 1}")
+
+numberedIngredients = (unlines [
+     "Recipe IcedMocha\r",
+     "1- 60`mL Chocolate\r",
+     "2- 60`mL Espresso [mix]\r",
+     "3- 30`mL Milk\r"],
+     "")
 ----
-singleIngredientNoIndex = "- 30`mL Steamed_Milk\r\n"
-
-singleIngredientNoIndex_output = "Ingredient {volume = 30, measure = \"ml\", ingredientName = \"Steamed_Milk\", index = Nothing}"
-
-test_ingredient = pTest "for valid single non-indexed ingredient" 
-     singleIngredientNoIndex_output $ runParse ingredient singleIngredientNoIndex
-
-----
-singleIngredientIndexed = "1- 30`mL Steamed_Milk\r\n"
-
-singleIngredientIndexed_output = "Ingredient {volume = 30, measure = \"ml\", ingredientName = \"Steamed_Milk\", index = 1}"
-
-test_ingredientIndexed = pTest "for valid single indexed ingredient" 
-     singleIngredientIndexed_output $ runParse ingredient singleIngredientIndexed
-
 tests = TestList [TestLabel "Recipe, simple" test_simpleInput
                  ,TestLabel "Ingredient, non-indexed" test_ingredient
                  ,TestLabel "Ingredient, indexed" test_ingredientIndexed
