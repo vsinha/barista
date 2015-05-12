b32b70e6467f43237460539f4ec50f2af50e7b5c
Viraj Sinha <virajosinha@gmail.com>
2015-05-12 15:18:47 -0400

    added Ingredient annotation to Types

--- Types.hs
+++ Types.hs
@@ -10,7 +10,10 @@ data Ingredient = Ingredient
      , measure :: Measure
      , ingredientName :: String
      , index :: (Maybe Int)
+     , annotations :: (Maybe [Annotation])
      } deriving Show
 
 type Measure = String
+data Annotation = MixSource | Hold | AspirateSpeed | DispenseSpeed 
+  deriving (Eq, Ord, Enum, Show)
 
